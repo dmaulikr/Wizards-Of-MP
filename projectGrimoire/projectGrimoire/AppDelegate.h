5d9fae41e7912ed1e64226a91d820f66efe69c8c
Prayaas Jain <prayaas.jain@pearson.com>
2016-03-30 13:30:49 -0700

    Initializing repo

---
+++ projectGrimoire/projectGrimoire/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  projectGrimoire
+//
+//  Created by Prayaas Jain on 3/30/16.
+//  Copyright © 2016 Prayaas Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
